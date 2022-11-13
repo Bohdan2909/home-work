5c7b3cee74d07487def7081671e05b5c99646de8
bogdanatamanuk <0997393654b@gmail.com>
2022-11-13 01:35:38 +0100

    finished task 5 modified

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -8,7 +8,7 @@ import JuniorPlus from './pages/JuniorPlus'
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
     JUNIOR: '/junior',
-    JUNIOR_PLUS: '/junior-plus',
+    JUNIOR_PLUS: '/junior_plus',
 }
 
 function Pages() {
@@ -18,14 +18,12 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
-
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
-
+                <Route path={'/'} element={<PreJunior/>} />
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>} />
                 <Route path={PATH.JUNIOR} element={<Junior/>} />
                 <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>} />
-                <Route path={'/'} element={<PreJunior/>} />
                 <Route path={'*'} element={<Error404/>} />
+
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
 
