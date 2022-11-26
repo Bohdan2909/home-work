6c4de003eb23d9794ffb54b25b7c6b2884c44432
bogdanatamanuk <0997393654b@gmail.com>
2022-11-26 23:17:18 +0100

    finished task 7

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import { restoreState, saveState } from './localStorage/localStorage'
 import s2 from '../../s1-main/App.module.css'
@@ -19,7 +19,6 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
         setValue( restoreState('hw6-editable-span-value', value))    }
 
 
