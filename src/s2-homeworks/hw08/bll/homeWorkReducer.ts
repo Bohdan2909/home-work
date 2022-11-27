d705ba593c9bfbe2e2f92b2afcfb42d94f48b981
bogdanatamanuk <0997393654b@gmail.com>
2022-11-27 21:29:28 +0100

    finished task 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,26 @@
 import {UserType} from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+    | {
+    type: 'sort';
+    payload: 'up' | 'down'
+}
+    | {
+    type: 'check';
+    payload: number
+}
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            let copyState = [...state]
+            return copyState.sort((a, b) => action.payload === 'up' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name))
+            // need to fix
         }
-        case 'check': {
 
-            return state // need to fix
+        case 'check': {
+            return state.filter(el => el.age > 18)
+            // need to fix
         }
         default:
             return state
