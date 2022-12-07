f337954acca6d8ad48c84209f450feb8f8f26ce1
bogdanatamanuk <0997393654b@gmail.com>
2022-12-07 15:28:11 +0100

    finished task 9 refactor

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -38,14 +38,16 @@ function Clock() {
     const minute = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
     const hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
     const seconds = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()
+    const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
+    const month = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
     const stringTime = `${hours}:${minute}:${seconds}` || <br/>
     // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     //часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = `${day}.${month}.${date.getFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = date.getDate() || <br/> // пишут студенты
-    const stringMonth = date.getMonth() + 1 || <br/> // пишут студенты
+    const stringDay = date.toLocaleString("en-US", {  weekday: 'long' }) || <br/> // пишут студенты
+    const stringMonth = date.toLocaleString("en-US", { month: 'long' }) || <br/> // пишут студенты
 
     return (
         <div className={s.clock}>
